c208e92a8312498e9f4de015a11041654aa3afbd
Ajay Dhiman <goodluckajay@gmail.com>
2020-10-04 17:37:56 +0530

    Add files via upload

---
+++ integration11/src/main/java/com/syne/PrintService.java
@@ -0,0 +1,12 @@
+package com.syne;
+
+import org.springframework.messaging.Message;
+
+public class PrintService {
+	
+	public void print(Message<String> message)
+	{
+		System.out.println(message.getPayload());
+	}
+
+}
